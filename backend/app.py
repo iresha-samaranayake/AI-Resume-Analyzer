52e14720e20d3386136fd58fbf4a1e6ccb00eb66
iresha1234-afk <ireshasamaranayake104@gmail.com>
2026-06-08 20:48:24 +0530

    Update app.py
    Add health check and resume upload endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
+##
 app = Flask(__name__)
 
+
+## Basic route to test if the server is running
 @app.route('/')
 def home():
-    return "AI Resume Analyzer Backend Running"
+    return jsonify({ ## jsonify is used to reaturn a JSON response same as return {"message": "AI Resume Analyzer Backend Running", "status": "success"}
+        "message": "AI Resume Analyzer Backend Running",
+        "status": "success"
+    })
+
+
+## Health check endpoint
+@app.route('/health')
+def health():
+    return {
+        "status": "healthy",
+        "server": "running"
+    }
+
+## Endpoint to handle resume uploads
+@app.route('/upload', methods=['POST'])
+def upload_resume():
+
+    if 'file' not in request.files:
+        return jsonify({
+            "error": "No file uploaded"
+        }), 400
+
+    return jsonify({
+        "message": "File received successfully"
+    })
 
 
 if __name__ == '__main__':
