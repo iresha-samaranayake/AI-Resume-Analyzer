05807c65e2a8cfeb395dd9c2ba3b12c9d3c5083a
iresha1234-afk <ireshasamaranayake104@gmail.com>
2026-06-09 12:04:04 +0530

    Create roles.py
    add data to role data set

---
+++ backend/data/roles.py
@@ -0,0 +1,34 @@
+ROLE_SKILLS = {
+    "Data Science Intern": [
+        "python",
+        "machine learning",
+        "tensorflow",
+        "sql"
+    ],
+
+    "Machine Learning Intern": [
+        "python",
+        "machine learning",
+        "tensorflow"
+    ],
+
+    "Frontend Developer": [
+        "html",
+        "css",
+        "javascript",
+        "react"
+    ],
+
+    "Java Developer": [
+        "java",
+        "spring boot",
+        "mysql"
+    ],
+
+    "UI/UX Designer": [
+        "ui/ux design",
+        "user research",
+        "wireframing",
+        "prototyping"
+    ]
+}
