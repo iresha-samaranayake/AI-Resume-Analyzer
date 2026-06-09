b1b3f29446e2da1d5cab97843cb7f8255d8ec5c0
iresha1234-afk <ireshasamaranayake104@gmail.com>
2026-06-09 12:04:32 +0530

    Create skills.py
    create skill data separately to keep code clean

---
+++ backend/data/skills.py
@@ -0,0 +1,27 @@
+SKILLS = [
+    "python",
+    "java",
+    "sql",
+    "html",
+    "css",
+    "javascript",
+    "react",
+    "spring boot",
+    "mysql",
+    "machine learning",
+    "tensorflow",
+    "data structures",
+    "algorithms",
+    "c++",
+
+    # UI/UX
+    "ui/ux design",
+    "user research",
+    "usability testing",
+    "wireframing",
+    "prototyping",
+
+    # General
+    "project management",
+    "leadership"
+]
